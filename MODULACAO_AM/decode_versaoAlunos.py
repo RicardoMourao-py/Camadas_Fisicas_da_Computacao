66b155fbf1eec4338f3f843d189434f2799a3619
Ricardo Mourão <ricardomrf@al.insper.edu.br>
2022-06-08 18:52:11 -0300

    Finaliza

--- MODULACAO_AM/decode_versaoAlunos.py
+++ MODULACAO_AM/decode_versaoAlunos.py
@@ -10,12 +10,8 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import time
 import peakutils
-
-#funcao para transformas intensidade acustica em dB
-def todB(s):
-    sdB = 10*np.log10(s)
-    return(sdB)
-
+import soundfile as sf
+from funcoes_LPF import *
 
 def main():
  
@@ -31,103 +27,44 @@ def main():
     duration = 3#tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
 
 
-    # faca um printo na tela dizendo que a captacao comecará em n segundos. e entao 
-    print('A captação começará em 5 segundos')
-    #use um time.sleep para a espera
-    time.sleep(5)
-    #faca um print informando que a gravacao foi inicializada
-    print('começou')
-    #declare uma variavel "duracao" com a duracao em segundos da gravacao. poucos segundos ... 
-    #calcule o numero de amostras "numAmostras" que serao feitas (numero de aquisicoes)
-    numAmostras = duration*fs
-    audio = sd.rec(int(numAmostras), fs, channels=1)
+    #lendo arquivo de áudio gerado pelo encode
+    audio, samplerate = sf.read('arquivo.wav')
+
+    #emitindo som áudio gerado pelo encode
+    print("áudio modulado")
+    sd.play(audio, fs)
     sd.wait()
-    print("...     FIM")
+
+    #demodulando áudio com transmissora de 13.000Hz
+    print("Demodulando áudio\n")
+    sinal = decode.generateSin(13000, 1, duration, fs)[1]
+    audioDemodulado = sinal*audio    
+
     
-    #analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista ...
-    #grave uma variavel com apenas a parte que interessa (dados)
-    y = audio[:,0] # informações de onda
+    #filtrando frequências superiores a 2.500Hz
+    print("Filtrando passa baixa\n")
+    audioFiltrado = LPF(audioDemodulado, 2500, fs)
 
-    # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
-    #t = np.linspace(inicio,fim,numPontos)
+    print("Reproduz áudio demodulado e filtrado")
+    sd.play(audioFiltrado, fs)
+    sd.wait()
 
-    # plot do gravico  áudio vs tempo!
-   
-    
-    ## Calcula e exibe o Fourier do sinal audio. como saida tem-se a amplitude e as frequencias
-    xf, yf = decode.calcFFT(y, fs)
-    #plt.figure("F(y)")
-    #plt.plot(xf,yf)
-    #plt.grid()
-    #plt.title('Fourier audio')
-    
-
-    #esta funcao analisa o fourier e encontra os picos
-    #voce deve aprender a usa-la. ha como ajustar a sensibilidade, ou seja, o que é um pico?
-    #voce deve tambem evitar que dois picos proximos sejam identificados, pois pequenas variacoes na
-    #frequencia do sinal podem gerar mais de um pico, e na verdade tempos apenas 1.
-   
-
-    index = peakutils.indexes(np.abs(yf), thres=0.2, min_dist=100) # encontra os indices numericos dos picos
-
-
-    picos_teclas = np.unique(list(decode.dict_teclas.values())) # 6
-    
-    dic = {}
-    for freq in xf[index]:
-        print(freq)
-        distancias = []
-        for pico_tecla in picos_teclas:
-            delta = np.abs(freq - pico_tecla)
-            distancias.append(delta)
-
-        x = distancias.index(min(distancias))
-        dic[picos_teclas[x]] = min(distancias)
-
-
-
-    cont = 0
-    tom = []    
-    for i in sorted(dic, key = dic.get):
-        if cont == 2:
-            break
-        tom.append(i)
-        cont+=1
-
-    print(tom)
-    
-    
-    for i in decode.dict_teclas.keys():
-        if decode.dict_teclas[i] == (tom[0], tom[1]) or decode.dict_teclas[i] == (tom[1], tom[0]):
-            print(f'Sua tecla é: {i}')
-            tecla = i
-
-    x = np.linspace(0.0, duration, numAmostras)
-    plt.figure(figsize=(25,10))
-    
-    plt.plot(x, audio)
-    plt.title('Áudio Gravado')
-    plt.xlabel('Tempo')
-    plt.ylabel('Amplitude')
-    #plt.xlim(0,1500)
-    plt.show()
-    #printe os picos encontrados! 
-    
-    #encontre na tabela duas frequencias proximas às frequencias de pico encontradas e descubra qual foi a tecla
-    #print a tecla.
-    
-    
-    x,y = decode.calcFFT(audio[:,0], fs)
+    ##### GRÁFICOS #######
+    x,y = decode.calcFFT(audioDemodulado, fs)
     plt.figure(figsize=(25,10))
     plt.plot(x, y)
-    plt.title(f'Tecla: {tecla}')
-    plt.xlabel('Frequencias')
-    plt.ylabel('Transformada')
-    #plt.xlim(0,1500)
-    #plt.axis([0, 1500, 0, 0.0001])
+    plt.title(f'Sinal de áudio demodulado - domínio da frequência')
+    plt.xlabel('Frequencies')
+    plt.ylabel('Amplitude')
     plt.show()
-    ## Exibe gráficos
-    #plt.show()
 
+    x,y = decode.calcFFT(audioFiltrado, fs)
+    plt.figure(figsize=(25,10))
+    plt.plot(x, y)
+    plt.title(f'Sinal de áudio filtrado - domínio da frequência')
+    plt.xlabel('Frequencies')
+    plt.ylabel('Amplitude')
+    plt.show()
+    
 if __name__ == "__main__":
     main()
--- MODULACAO_AM/encode_versaoAlunos.py
+++ MODULACAO_AM/encode_versaoAlunos.py
@@ -1,72 +1,96 @@
 
 #importe as bibliotecas
+from base64 import encode
 from suaBibSignal import *
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
-
-#funções a serem utilizadas
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
-
-#converte intensidade em Db, caso queiram ...
-def todB(s):
-    sdB = 10*np.log10(s)
-    return(sdB)
-
-
-
+import time
+from funcoes_LPF import *
+import soundfile as sf
 
 def main():
-    
-   
-    #********************************************instruções*********************************************** 
-    # seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada
-    # então inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF
-    # agora, voce tem que gerar, por alguns segundos, suficiente para a outra aplicação gravar o audio, duas senoides com as frequencias corresposndentes à tecla pressionada, segundo a tabela DTMF
-    # se voce quiser, pode usar a funcao de construção de senoides existente na biblioteca de apoio cedida. Para isso, você terá que entender como ela funciona e o que são os argumentos.
-    # essas senoides tem que ter taxa de amostragem de 44100 amostras por segundo, entao voce tera que gerar uma lista de tempo correspondente a isso e entao gerar as senoides
-    # lembre-se que a senoide pode ser construída com A*sin(2*pi*f*t)
-    # o tamanho da lista tempo estará associada à duração do som. A intensidade é controlada pela constante A (amplitude da senoide). Seja razoável.
-    # some as senoides. A soma será o sinal a ser emitido.
-    # utilize a funcao da biblioteca sounddevice para reproduzir o som. Entenda seus argumento.
-    # grave o som com seu celular ou qualquer outro microfone. Cuidado, algumas placas de som não gravam sons gerados por elas mesmas. (Isso evita microfonia).
-    
-    # construa o gráfico do sinal emitido e o gráfico da transformada de Fourier. Cuidado. Como as frequencias sao relativamente altas, voce deve plotar apenas alguns pontos (alguns periodos) para conseguirmos ver o sinal
-    
-
-    print("Inicializando encoder")
-    print("Aguardando usuário")
+    encode = signalMeu()    
     fs = 44100
-    encode = signalMeu()
-    tecla = input("Digite um número de entre 0 e 9, ou A, B, C, D, X, #: ")
-    freq1, freq2 = encode.dict_teclas[str(tecla)][0], encode.dict_teclas[str(tecla)][1] 
-    senoide1, senoide2 = encode.generateSin(freq1, 1, 5, fs)[1], encode.generateSin(freq2, 1, 5, fs)[1]
-    t1, t2 = encode.generateSin(freq1, 1, 5, fs)[0], encode.generateSin(freq2, 1, 5, fs)[0]
-    sinal = senoide1+senoide2
-    print("Gerando Tons base")
-    print("Executando as senoides (emitindo o som)")
-    #print("Gerando Tom referente ao símbolo : {}".format(NUM))
-    sd.play(sinal, fs)
     
+    sd.default.samplerate = fs
+    sd.default.channels = 2  
+    duration = 3
+
+    # faca um printo na tela dizendo que a captacao comecará em n segundos. e entao 
+    print('A captação começará em 5 segundos')
+    #use um time.sleep para a espera
+    time.sleep(5)
+    #faca um print informando que a gravacao foi inicializada
+    print('começou')
     
-    # Exibe gráficos
-    #plt.show()
-    # aguarda fim do audio
+    numAmostras = duration*fs
+    audio = sd.rec(int(numAmostras), fs, channels=1)
     sd.wait()
-    #plotFFT(self, signal, fs)
+    print("...     FIM")
     
-    # Exibe gráficos
+    # filtrando o audio
+    t, sinal = encode.generateSin(13000, 1, duration, fs)
+    y = audio[:,0]
+    audioFiltrado = LPF(y,2500,fs)
+
+    #emitindo som áudio filtrado
+    print("Áudio filtrado no passa baixa")
+    sd.play(audioFiltrado, fs)
+    sd.wait()
+
+    #modulando áudio com transmissora de 13.000Hz
+    audioModulado = sinal*audioFiltrado
+    
+    #normalizando áudio (dividir pela amplitude)
+    audioNormalizado = audioModulado/(np.max(np.abs(audioModulado)))
+    
+    #criando arquivo de áudio
+    sf.write('arquivo.wav', audioNormalizado, fs)
+
+    ##### GRÁFICOS #######
+
+    # Áudio filtrado no domínio da frequência
+    x,y = encode.calcFFT(audioFiltrado, fs)
     plt.figure(figsize=(25,10))
-    plt.plot(t1, senoide1, label=f"Senoide 1")
-    plt.plot(t2, senoide2, label=f"Senoide 2")
-    plt.plot(t2, sinal, label="Soma das senoides")
-    plt.title(f"Tecla {tecla}")
-    plt.xlabel("Tempo (s)", fontsize = 18)
-    plt.ylabel("Função Senoidal", fontsize = 18)
-    plt.legend(loc='upper right', fontsize=18)
-    plt.axis([1, 1.01, -2, 2])
+    plt.plot(x, y)
+    plt.title(f'Sinal de áudio filtrado - domínio da frequência')
+    plt.xlabel('Frequencies')
+    plt.ylabel('Amplitude')
     plt.show()
+    # Áudio filtrado no domínio do tempo
+    plt.figure(figsize=(25,10))
+    plt.plot(t, audioFiltrado)
+    plt.title(f'Sinal de áudio - domínio do tempo')
+    plt.xlabel('Tempo')
+    plt.ylabel('Amplitude')
+    plt.show()
+    # Áudio modulado no domínio da frequencia
+    x,y = encode.calcFFT(audioModulado, fs)
+    plt.figure(figsize=(25,10))
+    plt.plot(x, y)
+    plt.title(f'Sinal de áudio modulado - domínio da frequência')
+    plt.xlabel('Frequencies')
+    plt.ylabel('Amplitude')
+    plt.show()
+    # Áudio modulado no domínio do tempo
+    plt.figure(figsize=(25,10))
+    plt.plot(t, audioModulado)
+    plt.title(f'Sinal de áudio modulado - domínio do tempo')
+    plt.xlabel('Tempo')
+    plt.ylabel('Amplitude')
+    plt.show()
+    # Áudio normalizado no domínio da frequência
+    x,y = encode.calcFFT(audioNormalizado, fs)
+    plt.figure(figsize=(25,10))
+    plt.plot(x, y)
+    plt.title(f'Sinal de áudio normalizado - domínio da frequência')
+    plt.xlabel('Frequencies')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+
 if __name__ == "__main__":
     main()
+    
